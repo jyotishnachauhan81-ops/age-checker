d6fac9cab43b328909ac7092b0963598ebe6b993
jyotishnachauhan81-ops <jyotishnachauhan81@gmail.com>
2026-03-22 23:46:43 +0530

    Add files via upload
    # Age checker program
    This is the simple python program that checks whether a person is eligible based on age.
    ## Features
    - Takes user input (age)
    - Checks condition using if-else
    - Display results
    ## How to run
    1. Open file in Python
    2. Run the program
    3. Enter your age
    
    ## Author
    Jyotishna Chauhan

---
+++ age_checker2 .py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age:"))
+
+if age < 0:
+    print("invalid age")
+elif age >= 18:
+    print("you are eligible")
+else:
+    print("you are not eligible")
+          
+    
