74c4f6b8407c30a5e0aca84f1b565013a7ba35d6
jyotishnachauhan81-ops <jyotishnachauhan81@gmail.com>
2026-03-23 23:49:44 +0530

    Add files via upload
    * How it works
    - Takes 3 numbers as input
    - Uses if-elif-else to compare
    - Display the largest
    *Purpose
    Beginner practice for:
    - Conditional statements
    - Logical operators
    
    AUTHOR
    Jyotishna chauhan

---
+++ first largest of numbers .py
@@ -0,0 +1,11 @@
+a= int(input("enter first number:"))
+b= int(input("enter second number:"))
+                         
+c= int(input("enter third number:"))
+if a>=b and a>=c:
+    print("Largest is :",a)
+elif b>=a and b>=c:
+    print("largest is:",b)
+else:
+    print("largest is:",c)
+    
