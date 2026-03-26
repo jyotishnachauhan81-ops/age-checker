cf9998cb505348c8fc866bf9027fa73a0ac06b3f
jyotishnachauhan81-ops <jyotishnachauhan81@gmail.com>
2026-03-26 21:31:48 +0530

    Add files via upload

---
+++ simple calculator .py
@@ -0,0 +1,22 @@
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+print("1.add 2.subtract 3.multiply 4.divide")
+choice =int(input("choose operation:"))
+if choice == 1:
+    print("result:",a + b)
+elif choice == 2:
+    print("result:",a - b)
+elif choice ==3:
+    print("result:",a * b)
+elif choice ==4:
+    print("result:",a / b)
+else:
+    ("invalid invoive")
+    
+      
+           
+
+    
+    
+
+    
